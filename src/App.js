b8053492aff179804cf0ec0740316c3d55186d1b
Filip Buchwald <fixu1998@gmail.com>
2022-09-08 17:44:10 +0200

    toggle task done interaction

--- src/App.js
+++ src/App.js
@@ -24,32 +24,44 @@ function App() {
         setTasks(tasks => tasks.filter(tasks => tasks.id !== id));
     };
 
-    return (
-        <Container>
-            <Header
-                title="Lista zadań"
-            />
-            <Section
-                title="Dodaj nowe zadanie"
-                body={<Form />}
-            />
-            <Section
-                title="Lista zadań"
-                body={
-                    <Tasks
-                        tasks={tasks}
-                        hideDone={hideDone}
-                        removeTask={removeTask}
-                    />}
-                extraHeaderContent={
-                    <Buttons
-                        tasks={tasks}
-                        hideDone={hideDone}
-                        toggleHideDone={toggleHideDone}
-                    />}
-            />
-        </Container>
-    );
+    const toggleTaskDone = (id) => {
+        setTasks(tasks => tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, done: !task.done };
+            }
+            return task;
+        }));
+    };
+
+    
+
+return (
+    <Container>
+        <Header
+            title="Lista zadań"
+        />
+        <Section
+            title="Dodaj nowe zadanie"
+            body={<Form />}
+        />
+        <Section
+            title="Lista zadań"
+            body={
+                <Tasks
+                    tasks={tasks}
+                    hideDone={hideDone}
+                    removeTask={removeTask}
+                    toggleTaskDone={toggleTaskDone}
+                />}
+            extraHeaderContent={
+                <Buttons
+                    tasks={tasks}
+                    hideDone={hideDone}
+                    toggleHideDone={toggleHideDone}
+                />}
+        />
+    </Container>
+);
 }
 
 export default App;
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,13 +1,16 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideDone, removeTask}) => (
+const Tasks = ({tasks, hideDone, removeTask, toggleTaskDone}) => (
   <ul className="list">
     {tasks.map(task => (
       <li
         key={task.id}
         className={`list__item ${task.done && hideDone ? "list__task--hidden" : ""}`}
       >
-        <button className=" list__button list__button--done">
+        <button 
+        className=" list__button list__button--done"
+        onClick={() => toggleTaskDone(task.id)}
+        >
           {task.done ? "✓" : ""}
         </button>
         <span className={`list__task ${task.done ? " list__task--done" : ""}`}>
