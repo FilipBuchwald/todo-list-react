b6ad1b04909a1cd127542986b4d5f85e2311284e
Filip Buchwald <fixu1998@gmail.com>
2022-11-01 15:44:50 +0100

    persist tasks in local storage - not working properly

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,9 +1,11 @@
-import { takeEvery, call, put } from "redux-saga/effects";
+import { call, put, delay, takeLatest, takeEvery, select } from "redux-saga/effects";
 import { getExampleTasks } from "./getExampleTasks";
-import { fetchExampleTasks, setTasks } from "./tasksSlice";
+import { saveTasksInLocalStorage } from "./tasksLocalStorage";
+import { fetchExampleTasks, selectTasks, setTasks } from "./tasksSlice";
 
 function* fetchExampleTasksHandler() {
   try {
+    yield delay(2000)
     const exampleTasks = yield call(getExampleTasks);
     yield put(setTasks(exampleTasks));
   } catch (error) { 
@@ -11,7 +13,14 @@ function* fetchExampleTasksHandler() {
   }
 }
 
+function* saveTasksInLocalStorageHandler () {
+  const tasks = yield select(selectTasks); // select() - pobieranie ze stora
+  yield call(saveTasksInLocalStorage, tasks);
+}
+
 export function* watchFetchExamplesTasks() {
-  console.log("Saga działa");
-  yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
+  console.log("Saga działa"); 
+  yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
+  yield takeEvery("*", saveTasksInLocalStorageHandler); //cokolwiek się stanie, odpali się saveTasks....
+
 }
