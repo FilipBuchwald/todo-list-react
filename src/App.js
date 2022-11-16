01027d77ffc13021bf7629942ea0d7fb57d98667
Filip Buchwald <fixu1998@gmail.com>
2022-11-16 17:26:26 +0100

    Added browser router to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
+import Author from "./features/author/Author";
 import Tasks from "./features/tasks/Tasks";
 
-const App = () => <Tasks />;
+const App = () => (
+  <BrowserRouter>
+    <nav>
+      <ul>
+        <li>
+          <Link to="/zadania">
+            Zadania
+          </Link>
+        </li>
+        <li>
+          <Link to="/author">
+            O autorze
+          </Link>
+        </li>
+      </ul>
+      <Routes>
+        <Route path="/zadania" element={<Tasks />} />
+        <Route path="/author" element={<Author />} />
+      </Routes>
+    </nav>
+  </BrowserRouter>
+);
 
 export default App;
